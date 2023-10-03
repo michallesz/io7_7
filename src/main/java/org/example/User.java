0cfe28ff981c2e8be8dfec0fac83dacbae9a934e
student <student@pollub.edu.pl>
2023-10-03 17:36:15 +0200

    added class save user

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -4,4 +4,8 @@ public class User {
     public void printUser(String name){
         System.out.println(name);
     }
+
+    public void saveUser(String name){
+        System.out.println("Zapisuje usera: " + name);
+    }
 }
